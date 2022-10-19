22613771d02b2b16f17cf331bf0b230739013bd6
h4 <20080114+tan-i-ham@users.noreply.github.com>
2022-10-19 09:21:53 -0400

    Time: 11 ms (67.92%), Space: 45.3 MB (37.78%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int N = intervals.length;
+        
+        PriorityQueue<Integer> endTimeMinHeap = new PriorityQueue<>();
+        endTimeMinHeap.add(intervals[0][1]); // [30]
+        for(int i=1; i< N; i++){
+            int startTime = intervals[i][0];
+            int endTime = intervals[i][1];
+            if(startTime >= endTimeMinHeap.peek()){
+                endTimeMinHeap.poll(); //  [30]
+            }
+            endTimeMinHeap.offer(endTime); //[10,30] -> [15,30]
+        }
+        return endTimeMinHeap.size();
+    }
+}
