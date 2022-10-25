a2d0046e06a449e0e509ae88feb5e12baa7eff4a
h4 <20080114+tan-i-ham@users.noreply.github.com>
2022-10-25 07:51:38 -0400

    Time: 1 ms (99.79%), Space: 42.1 MB (98.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,14 @@
 // naive solution: 2 for loops
-// 
+// improve: use a map to record the (target - nums[i])'s and index
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int N = nums.length;
+        Map<Integer, Integer> map = new HashMap<>(); // key is the counterpart, value is the idx
         for(int i=0; i< N; i++){
-            for(int j=i+1; j< N; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i, j};
-                }
+            if(map.containsKey(nums[i])){
+                return new int[]{map.get(nums[i]), i};
             }
+            map.put(target - nums[i], i);
         }
         return null;
     }
