a7f959c9ca12446c985d0ed28531f08286c743db
h4 <20080114+tan-i-ham@users.noreply.github.com>
2022-11-08 22:33:58 -0500

    Time: 145 ms (5.12%), Space: 124.7 MB (5.04%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -0,0 +1,53 @@
+// min[i][j] = minimum number from i to j
+// max[i][j] = maximum number from i to j
+// ans = sum (max[i][j] - min[i][j])
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        
+        int N = nums.length;
+        int[][] min = new int[N][N];
+        for(int s=0; s < N; s++){
+            for(int e=s; e < N; e++){
+                if(s==e){
+                    min[s][e] = nums[e];
+                }else{
+                    min[s][e] = Math.min(min[s][e-1], nums[e]);
+                }
+            }
+        }
+        int[][] max = new int[N][N];
+        for(int s=0; s < N; s++){
+            for(int e=s; e < N; e++){
+                if(s==e){
+                    max[s][e] = nums[e];
+                }else{
+                    max[s][e] = Math.max(max[s][e-1], nums[e]);
+                }
+            }
+        }      
+        long ans = 0;
+        for(int s=0; s < N; s++){
+            for(int e=s; e < N; e++){
+                ans += (max[s][e] - min[s][e]);
+            }
+        }
+        return ans;
+    }
+}
+
+// [4,-2,-3,4,1]
+// min=[
+//     [4,-2,-3,-3,-3],
+//     [0,-2,-3,-3,-3],
+//     [0,0,-3,-3,-3],
+//     [0,0,0,4,1],
+//     [0,0,0,0,1]
+// ]
+
+// max=[
+//     [4,4,4,4,4],
+//     [0,-2,-2,4,4],
+//     [0,0,-3,4,4],
+//     [0,0,0,4,4],
+//     [0,0,0,0,1]
+// ]
