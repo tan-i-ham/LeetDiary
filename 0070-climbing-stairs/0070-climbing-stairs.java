07ee2aeeb34698d79c6c8108bab14f69d1f926f2
h4 <20080114+tan-i-ham@users.noreply.github.com>
2022-11-17 20:47:29 -0500

    Time: 0 ms (100.00%), Space: 39 MB (89.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,24 +1,21 @@
 // backtrack, base case, 0 return 1
 // n=45, TLE
 // use map to store the counted value, e.g. key is n, value is the ways to climb to the top
+// dp[i] = the ways to climb to the i-th step
 class Solution {
-    Map<Integer, Integer> map = new HashMap<>();
-   
+    
     public int climbStairs(int n) {
-         
-        if(n<0){
-            return 0;
-        }
-        if(map.containsKey(n)){
-            return map.get(n);
-        }
         if(n==0){
-            map.put(0,1);
             return 1;
+        }        
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<= n; i++ ){
+            // choose either climb 1 step or 2 steps
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        // choose either climb 1 step or 2 steps
-        int ans = climbStairs(n-1) + climbStairs(n-2);
-        map.put(n, ans);
-        return ans;
+
+        return dp[n];
     }
 }
