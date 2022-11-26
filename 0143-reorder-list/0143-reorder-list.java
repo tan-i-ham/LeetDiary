a520927b98f18638ec49963cda53d2d05b590d9e
h4 <20080114+tan-i-ham@users.noreply.github.com>
2022-11-26 09:03:05 -0500

    Time: 10 ms (29.42%), Space: 54 MB (56.85%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -13,16 +13,18 @@ class Solution {
         ListNode dummyHead = new ListNode(-1, head);
         ListNode fast = dummyHead;
         ListNode slow = dummyHead;
+        // 1. find the middle node first by using fast and slow technique
         while(fast != null && fast.next!=null){
             fast = fast.next.next;
             slow = slow.next;
         }
         
         ListNode l2 = slow.next;
-        slow.next = null;
+        slow.next = null; // split the first and second half, important!!!
         
         ListNode curr = l2; // 3
         ListNode prev = null;
+        // 2. reverse the second half node
         while(curr!=null){
             ListNode temp = curr.next; // 4, null
             curr.next = prev; // 3-> n, 4->3->null
@@ -30,8 +32,10 @@ class Solution {
             curr = temp; // 4
         }
         l2 = prev;
+        
         ListNode c1 = head;
         ListNode c2 = l2;
+        // 3. connect each node one by one
         while(c1!=null && c2!=null){
             ListNode t1 = c1.next;
             ListNode t2 = c2.next;
@@ -40,6 +44,5 @@ class Solution {
             c1 = t1;
             c2 = t2;
         }
-        // return c1;
     }
 }
