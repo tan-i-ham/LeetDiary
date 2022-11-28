2cb88141bfe64f9112bc0448ca7f1c9f01a32a9c
h4 <20080114+tan-i-ham@users.noreply.github.com>
2022-11-28 09:15:52 -0500

    Time: 28 ms (87.45%), Space: 58.2 MB (76.21%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -8,21 +8,28 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         
         
+        // a + b + c = 0
         
         for(int i=0; i< N; i++){
-            if(i> 0 && nums[i]==nums[i-1]){
+            if(i> 0 && nums[i]==nums[i-1]){ // choose a
                 continue;
             }
-            Set<Integer> seen = new HashSet<>();
-            for(int j=i+1; j< N; j++){
-                int sum = nums[i] + nums[j];
-                if(seen.contains(-sum)){
-                    ans.add(Arrays.asList(nums[i], nums[j], -sum));
-                    while (j + 1 < N && nums[j] == nums[j + 1]){
-                        j++;
+            int l = i+1;
+            int r = N-1;
+
+            while(l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum < 0){
+                    l++;
+                }else if(sum > 0){
+                    r--;
+                }else{
+                    ans.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    while(l< r && nums[l] == nums[l-1]){
+                        l++;
                     }
                 }
-                seen.add(nums[j]);
             }
         }
         return ans;
