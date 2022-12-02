dc0f474b9e87f2ba7aa4156463191bd773a35b18
h4 <20080114+tan-i-ham@users.noreply.github.com>
2022-12-02 07:05:08 -0500

    Time: 1 ms (99.63%), Space: 39.6 MB (92.35%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,21 +1,28 @@
-// store every weight to maxHeap
-// pick 2 elemeents to compare and smash, the left stone push again into the maxHeap
-// return the left one if there is only one stone in the maxHeap
+// use maxHeap to store the stone
+// first put every stone to the maxHeap
+// pop 2 element if the size is >= 2
+// if same than no new element put into it
+// after smash, add the (m1 - m2) into maxHeap
 class Solution {
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        for(int stone: stones){
-            maxHeap.add(stone);
+        for(int s: stones){
+            maxHeap.add(s);
         }
-        while(maxHeap.size() >= 2){
-            int one = maxHeap.poll();
-            int two = maxHeap.poll();
-            maxHeap.add(Math.abs(one - two));
+        while(maxHeap.size()>=2){
+            int m1 = maxHeap.poll();
+            int m2 = maxHeap.poll();
+            if(m1 == m2){
+                continue;
+            }
+            maxHeap.add(m1-m2);
         }
-
-        return maxHeap.poll();
+        return maxHeap.size()==0 ? 0 : maxHeap.poll();
     }
 }
-
-// TC: O(NlogN)
-// SC: O(N)
+// [2,7,4,1,8,1]
+// maxHeap = [8,7,4,2,1,1]
+// maxHeap = [4,2,1,1,1]
+// maxHeap = [2,1,1,1]
+// maxHeap = [1,1,1]
+// maxHeap = [1]
