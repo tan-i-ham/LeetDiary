504726c99a5a73fae9912bd3777353b4332c9228
h4 <20080114+tan-i-ham@users.noreply.github.com>
2023-01-20 06:23:21 -0500

    Time: 462 ms (25.34%), Space: 49.1 MB (53.93%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,39 @@
 // TC: O(n^2)
 class Solution {
+    List<List<Integer>> ans;
     public List<List<Integer>> threeSum(int[] nums) {
         int N = nums.length;
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
+        ans = new ArrayList<>();
         
-        
-        // a + b + c = 0
         
         for(int i=0; i< N; i++){
-            if(i> 0 && nums[i]==nums[i-1]){ // choose a
+            if(i> 0 && nums[i]==nums[i-1]){ // choose a, to avoid duplicate triplets
                 continue;
             }
-            int l = i+1;
-            int r = N-1;
-
-            while(l < r){
-                int sum = nums[i] + nums[l] + nums[r];
-                if(sum < 0){
-                    l++;
-                }else if(sum > 0){
-                    r--;
-                }else{
-                    ans.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                    l++;
-                    while(l< r && nums[l] == nums[l-1]){ // increase the left pointer only, because the array is sorted, right point will be updated at line 24, e.g [-3,-2,-2,0,0,2,2], l will to first 0, the r will start to decrease
-                        l++;
-                    }
-                }
+            if(nums[i] > 0){
+                break; // because array is sorted, the remaining value after index i, the sum cannot be 0
             }
+            twoSum(-nums[i], i+1, nums);
         }
         return ans;
     }
+    
+    public void twoSum(int target, int startFrom, int[] nums){
+        Set<Integer> seen = new HashSet<>();
+        
+        for(int i=startFrom; i< nums.length; i++){
+            int complement = target - nums[i];
+            if(seen.contains(complement)){
+                ans.add(Arrays.asList(nums[startFrom-1], nums[i], complement));
+                while(i + 1 < nums.length && nums[i] == nums[i+1]){
+                    i++;
+                }
+            }
+            seen.add(nums[i]);
+        }
+    }
 }
+
+// TC: O(N^2)
+// SC: O(N) , the seen set
