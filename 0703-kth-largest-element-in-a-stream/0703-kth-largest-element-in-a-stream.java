9dd21c418227af7f2ca5cf4679dfaa30fc063ad4
h4 <20080114+tan-i-ham@users.noreply.github.com>
2023-01-25 08:19:57 -0500

    Time: 38 ms (11.28%), Space: 57.3 MB (5.23%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -1,31 +1,36 @@
 class KthLargest {
+    // create a minHeap, that has k elements only
+    // create a maxHeap to store the other elements when nums array has number of elements that larger than k
     PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
     int K;
+    
+    // the constructor
+    // TC: O(NlogN) , N is the length of given array
+    // SC: O(N)
     public KthLargest(int k, int[] nums) {
         minHeap = new PriorityQueue<>();
-        K = k;
+        // minHeap = new PriorityQueue<>( (a, b) -> Integer.compares(b, a));
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         
+        K = k;
         for(int num: nums){
-            if(minHeap.size() < k){
-                minHeap.offer(num);
-                continue;
-            }
-            if(minHeap.size()>=k && num > minHeap.peek()){
-                minHeap.poll();
-                minHeap.offer(num);
-            }
-            
+            add(num);
         }
     }
-    
+    // TC: O(logN)
+    // SC: O(N)    
     public int add(int val) {
         if(minHeap.size() < K){
-            minHeap.offer(val); 
-        }else if(val > minHeap.peek()){
-            minHeap.poll();
-            minHeap.offer(val);   
+            minHeap.add(val);    
+        }else{
+            if(minHeap.peek() < val){
+                maxHeap.add(minHeap.poll());
+                minHeap.add(val);
+            }else{
+                maxHeap.add(val);
+            }
         }
-            
         return minHeap.peek();
     }
 }
