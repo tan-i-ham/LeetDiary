3c0813a551915ff503f24d4b7d3a25f7bf0435de
h4 <20080114+tan-i-ham@users.noreply.github.com>
2023-02-08 11:37:43 -0500

    Time: 182 ms (10.79%), Space: 43 MB (48.55%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,25 +1,32 @@
-// i== currMaxRangeTo, when iterate to currMaxRangeTo, 
-// this means, we reach to the furthest idx in current step/jump (1),
-// we need to increase a step/jump (1->2), and update the latest idx we can reach for next jump (2)
 class Solution {
+    int[] memo;
     public int jump(int[] nums) {
+        memo = new int[10001];
+        Arrays.fill(memo, 10001);
         int N = nums.length;
         if(N==1){
             return 0;
         }
-        int currMaxRangeTo = 0;
-        int nextMaxRangeTo = 0;
-        int jumps = 0;
-        for(int i=0; i < N; i++){
-            nextMaxRangeTo = Math.max(nextMaxRangeTo, i + nums[i]);
-            if(i==currMaxRangeTo){
-			    if (currMaxRangeTo >= N - 1) {
-					break;
-                }                
-                currMaxRangeTo = nextMaxRangeTo;
-                jumps++;
-            }
+        return jump(nums, 0);
+    }
+    public int jump(int[] nums, int currIdx){
+        if(currIdx >= nums.length - 1){
+            return 0;
         }
-        return jumps;
+        if(memo[currIdx]!=10001){
+            return memo[currIdx];
+        }
+        int min = 10001;
+        for(int step=1; step <= nums[currIdx]; step++){
+            min = Math.min(min, 1 + jump(nums, currIdx+step));
+        }
+        memo[currIdx] = min;
+        return min;
     }
 }
+// before memo array
+// TC: O(N!)
+// SC: O(N)
+// after memo array
+// TC: O(N^2)
+// SC: O(N)
