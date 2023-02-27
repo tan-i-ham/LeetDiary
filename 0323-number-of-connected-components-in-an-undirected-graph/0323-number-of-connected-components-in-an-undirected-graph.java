8bb2b98ae3e36a73cfa610180bd1fbf5dcaf29d0
h4 <20080114+tan-i-ham@users.noreply.github.com>
2023-02-26 23:15:00 -0500

    Time: 1 ms (100.00%), Space: 43.2 MB (25.05%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,46 +1,52 @@
 class Solution {
-    int[] par;
-    int[] rank;
-    public int countComponents(int n, int[][] edges) {
-        par = new int[n];
-        for(int i=0; i< n; i++){
-            par[i] = i;
+    public class UnionFind {
+        int[] root;
+        int[] rank;
+
+        public UnionFind(int n){
+            root = new int[n];
+            rank = new int[n];
+
+            for(int i=0; i< n; i++){
+                root[i] = i;
+                rank[i] = 1;
+            }
         }
-        rank = new int[n];
-        Arrays.fill(rank, 1);
         
-        int ans = n;
-        for(int[] edge: edges){
-            ans -= union(edge[0], edge[1]);
+        public int find(int vertex){
+            if(vertex == root[vertex]){
+                return vertex;
+            }
+            return root[vertex] = find(root[vertex]);
         }
-        return ans;
+        
+        public int union(int v1, int v2){
+            int r1 = find(v1);
+            int r2 = find(v2);
+            if(r1 == r2){
+                return 0;
+            }
+            if(r1 != r2){
+                if(rank[r1] > rank[r2]){
+                    root[r2] = r1;
+                }else if(rank[r2] > rank[r1]){
+                    root[r1] = r2;
+                }else{
+                    root[r1] = r2;
+                    rank[r1]++;
+                }
+            }
+            return 1;
+        }
+        
     }
-    
-    public int find(int node){
-        if(node == par[node]){
-            return node;
+    public int countComponents(int n, int[][] edges) {
+        UnionFind uf = new UnionFind(n);
+        int components = n;
+        for(int[] e: edges){
+            components -= uf.union(e[0], e[1]);
         }
-        int res = node;
-        while(res != par[res]){
-            par[res] = par[par[res]];
-            res = par[res];
-        }
-        return res;
-    }
-    
-    public int union(int n1, int n2){
-        int p1 = find(n1);
-        int p2 = find(n2);
-        if(p1 == p2){
-            return 0;
-        }
-        if(rank[p1] >= rank[p2]){
-            par[p2] = p1;
-            rank[p1] += rank[p2];
-        }else{
-            par[p1] = p2;
-            rank[p2] += rank[p1];
-        }
-        return 1;
+
+        return components;
     }
 }
