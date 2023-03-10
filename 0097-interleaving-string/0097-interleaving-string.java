2656dd23184f5b63b261c941f481a1ee1be43577
h4 <20080114+tan-i-ham@users.noreply.github.com>
2023-03-10 10:55:44 -0500

    Time: 1 ms (94.23%), Space: 41.2 MB (26.47%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,22 +1,29 @@
 class Solution {
     int[][] memo;
+    int s1Len;
+    int s2Len;
+    int s3Len;
     public boolean isInterleave(String s1, String s2, String s3) {
-        memo = new int[s1.length()][s2.length()];
-        if(s1.length() + s2.length() != s3.length()){
+        s1Len = s1.length();
+        s2Len = s2.length();
+        s3Len = s3.length();
+        
+        memo = new int[s1Len][s2Len];
+        if(s1Len + s2Len != s3Len){
             return false;
         }
-        for (int i = 0; i < s1.length(); i++) {
-            for (int j = 0; j < s2.length(); j++) {
+        for (int i = 0; i < s1Len; i++) {
+            for (int j = 0; j < s2Len; j++) {
                 memo[i][j] = -1;
             }
         }
         return backtrack(s1,s2,s3,0,0);
     }
     public boolean backtrack(String s1, String s2, String s3, int i1, int i2){
-        if(i1 == s1.length()){
+        if(i1 == s1Len){
             return s2.substring(i2).equals(s3.substring(i1+i2));
         }
-        if(i2 == s2.length()){
+        if(i2 == s2Len){
             return s1.substring(i1).equals(s3.substring(i1+i2));
         }  
         
