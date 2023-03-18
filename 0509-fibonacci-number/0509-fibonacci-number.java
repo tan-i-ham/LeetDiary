4274e525b30f8ca92cc722a99cc3b2a98cf8284e
h4 <20080114+tan-i-ham@users.noreply.github.com>
2023-03-17 22:38:26 -0400

    Time: 0 ms (100.00%), Space: 39.4 MB (28.67%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,21 +1,16 @@
 class Solution {
-    int[] memo;
     public int fib(int n) {
         if(n==0 || n==1){
             return n;
         }
-        memo = new int[n+1];
-        memo[1] = 1;
-        return helper(n);
-    }
-    public int helper(int n){
-        if(n==0 || n==1){
-            return n;
+        int twoPrev = 0;
+        int prev = 1;
+        int curr = 0;
+        for(int i=2; i<=n; i++){
+            curr = prev + twoPrev;
+            twoPrev = prev;
+            prev = curr;
         }
-        if(memo[n]!=0){
-            return memo[n];
-        }
-        memo[n] = helper(n-2) + helper(n-1);
-        return memo[n];
+        return curr;
     }
 }
