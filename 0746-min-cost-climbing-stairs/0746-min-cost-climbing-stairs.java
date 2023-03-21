8266ad3293886840dc7c3682289aa66d14c209f8
h4 <20080114+tan-i-ham@users.noreply.github.com>
2023-03-21 08:27:10 -0400

    Time: 0 ms (100.00%), Space: 42.3 MB (39.58%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,24 +1,28 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int N = cost.length;
-        int[] dp = new int[N+1]; // dp[i]: the min cost to i th stair
 
-        
+        int ans = 0;
+        int twoSteps= 0;
+        int oneStep = 0;
         for(int i=2; i<= N; i++){
-            int oneStep = dp[i-2] + cost[i-2];
-            int twoSteps = dp[i-1] + cost[i-1];
-            dp[i] = Math.min(oneStep, twoSteps);
+            int temp = oneStep;
+            oneStep = Math.min(oneStep + cost[i-1], twoSteps + cost[i-2]);
+            twoSteps = temp;
         }
-        return dp[N];
+        return oneStep;
     }
 }
 
 // [1,100,1,1,1,100,1,1,100,1]
-// i=2, [0,0,1,..]
-// i=3, [0,0,1,2, ...]
-// i=4, [0,0,1,2,2,..]
-// i=5, [0,0,1,2,2,3,.]
-// i=6, [0,0,1,2,2,3,3, ]
-// i=7, [0,0,1,2,2,3,3,4 ]    
-// i=8, [0,0,1,2,2,3,3,4,4, ]    
-// i=9, [0,0,1,2,2,3,3,4,4,5]      
+//     0       0
+// i   oneStep twoSteps
+// 2   1       0
+// 3   2       1
+// 4   2       2
+// 5   3       2
+// 6   3       3  
+// 7   4       3
+// 8   4       4 
+// 9   5       4       
+// 10  6       5
